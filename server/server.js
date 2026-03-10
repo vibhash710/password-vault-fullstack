2c7e7d0f7c11f9360d372cfcf562572b8ba75aed
Vibhash Mishra <vibhashmishra710@gmail.com>
2026-03-10 11:35:18 +0530

    fix CORS to allow www and root domain

--- server/server.js
+++ server/server.js
@@ -15,7 +15,9 @@ app.set('trust proxy', 1);
 // ========== MIDDLEWARE ==========
 
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'  ? process.env.CLIENT_URL : 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production'
+    ? [process.env.CLIENT_URL, process.env.CLIENT_URL_ALT]
+    : ["http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
